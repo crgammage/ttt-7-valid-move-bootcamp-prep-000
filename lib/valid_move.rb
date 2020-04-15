913e595c02e923e90d6f8e585c2b6e333de3f525
masterful-array-3850 <crgammage@gmail.com>
2020-04-15 15:26:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] ==  "" || board[index] == nil
     return false
   end
-else board[index]
-
+  if board[index] == "X" || board[index] == "O"
+      return true
     end
 end
