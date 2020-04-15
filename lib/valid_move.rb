745c5e035db12081da42653dbb59b2df886780f3
masterful-array-3850 <crgammage@gmail.com>
2020-04-15 15:02:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,15 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken == true
+  if position_taken? == true
+
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken(board, index)
-  if board[index] == "" || " " || nil
-    return board[index] == "X"
-  elsif board[index] == "X" || "O"
-    return
+def position_taken?(board, index)
+  if board[index] == " " || board[index] ==  "" || board[index] == nil
+    return false
+  end
+  if board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
