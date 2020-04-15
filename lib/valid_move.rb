4c3898655f61726bd9176f742572263bdfb504bf
masterful-array-3850 <crgammage@gmail.com>
2020-04-15 15:36:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,8 @@ def valid_move?(board, index)
   if position_taken?(board, spot) == false &&  spot.between?(0, 8)
     return true
   end
+else
+  return true
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
